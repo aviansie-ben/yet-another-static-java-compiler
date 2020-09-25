f9d16144152cb59c133fa7d1abccee104eca0ecf
Benjamin Thomas (Aviansie Ben) <ben@benthomas.ca>
2020-09-25 14:02:14 +0000

    Disable llc optimizations in compile.sh

--- test/compile.sh
+++ test/compile.sh
@@ -4,6 +4,5 @@ cd $(dirname $(realpath "$0"))/out
 set -e
 
 cargo run --manifest-path ../../compiler/Cargo.toml -- .:../../jcl/out:/usr/lib/jvm/java-8-openjdk/jre/lib/rt.jar "$1"
-llc -O2 test.bc
+llc -O0 test.bc
 clang -no-pie -L../../rt/target/debug -lmocha_rt test.s -o test
-
