502d13fcfffa400b0c9570bc32bfdcc6a56b9786
Benjamin Thomas (Aviansie Ben) <ben@benthomas.ca>
2020-09-25 14:05:08 +0000

    Add temporary fix for line 0 debuginfo
    As it turns out, for whatever reason, LLVM is currently emitting some
    instructions with debuginfo specifying that they come from line 0 of the
    file containing the function. This mostly seems to occur for
    instructions introduced during register allocation performing spills and
    restores. This can sometimes confuse GDB and cause it to think that the
    current function has no debuginfo available after single-stepping.
    
    To prevent this from impacting debugging, a temporary hack has been
    added to compile.sh that will remove any debug locations pointing at
    line 0.

--- test/compile.sh
+++ test/compile.sh
@@ -5,4 +5,11 @@ set -e
 
 cargo run --manifest-path ../../compiler/Cargo.toml -- .:../../jcl/out:/usr/lib/jvm/java-8-openjdk/jre/lib/rt.jar "$1"
 llc -O0 test.bc
+
+# Currently, LLVM seems to sometimes emit debuginfo pointing at line 0 for instructions that are
+# materialized internally during code generation, e.g. during register allocation. This can cause
+# gdb to get confused and think that the function has no debuginfo available after single-stepping
+# into such code, so remove these before linking.
+sed -i '/\.loc\s\+[0-9]\+\s\+0\s\+0/d' test.s
+
 clang -no-pie -L../../rt/target/debug -lmocha_rt test.s -o test
